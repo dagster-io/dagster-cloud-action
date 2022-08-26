db0f1a7cf373b9ca9e80aa645f3253a8ed305979
prha <prha@elementl.com>
2022-08-26 11:11:44 -0700

    fix template

--- src/copy_template.sh
+++ src/copy_template.sh
@@ -4,9 +4,9 @@ if [ -z $CUSTOM_BASE_IMAGE_ALLOWED ] || [ -z $INPUT_BASE_IMAGE ]; then
     if [ ! -z $INPUT_BASE_IMAGE ]; then
         echo "Custom base images are not enabled for this organization, defaulting to python:3.8-slim."
     fi
-    echo "FROM python:3.8-slim\n" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
+    echo "FROM python:3.8-slim" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
 else
-    echo "FROM ${INPUT_BASE_IMAGE}\n" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
+    echo "FROM ${INPUT_BASE_IMAGE}" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
 fi
 
 cat /Dockerfile.template >> ${INPUT_TARGET_DIRECTORY}/Dockerfile
