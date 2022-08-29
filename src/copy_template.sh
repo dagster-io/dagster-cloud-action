5e82f8f9a788ae11d10f2b4195a51e2e07337af1
prha <1040172+prha@users.noreply.github.com>
2022-08-29 14:02:59 -0700

    enable custom base images (#28)
    * custom base images
    
    * test image before pushing
    
    * fix template
    
    * cosmetic change
    
    * change to 0.1.5
    
    * add tests for custom base image

--- src/copy_template.sh
+++ src/copy_template.sh
@@ -1,4 +1,14 @@
 #!/bin/bash -
 
-cp /Dockerfile.template ${INPUT_TARGET_DIRECTORY}/Dockerfile
+if [ -z $CUSTOM_BASE_IMAGE_ALLOWED ] || [ -z $INPUT_BASE_IMAGE ]; then
+    if [ ! -z $INPUT_BASE_IMAGE ]; then
+        echo "Custom base images are not enabled for this organization, defaulting to python:3.8-slim."
+    fi
+    echo "FROM python:3.8-slim" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
+else
+    echo "FROM ${INPUT_BASE_IMAGE}" > ${INPUT_TARGET_DIRECTORY}/Dockerfile
+fi
+
+
+cat /Dockerfile.template >> ${INPUT_TARGET_DIRECTORY}/Dockerfile
 cat $(python /expand_env_vars.py) >> ${INPUT_TARGET_DIRECTORY}/Dockerfile
