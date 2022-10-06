7693c3cd3f4c764be63d663761405666b8d73b1c
Shalabh Chaturvedi <shalabh@elementl.com>
2022-10-06 11:19:17 -0700

    simple hack to avoid depending on unreleased cloud cli

--- pex/src/builder/code_location.py
+++ pex/src/builder/code_location.py
@@ -1,5 +1,4 @@
 from audioop import add
-import base64
 import logging
 import pprint
 import sys
@@ -12,9 +11,16 @@ from . import util
 # published version.
 def add_or_update_code_location(deployment_name, location_name, **location_kwargs):
     with util.graphql_client("prod") as client:
+        
+        # config_utils can't validate a location with pex_tag (yet). once dagster-cloud-cli is
+        # published with the pex_tag changes, we don't need to do hack inject the 'pex_tag'.
+        pex_tag = location_kwargs.pop('pex_tag') if 'pex_tag' in location_kwargs else None
         location_document = config_utils.get_location_document(
             location_name, location_kwargs
         )
+        if pex_tag:
+            location_document["pex_tag"] = pex_tag
+
         gql.add_or_update_code_location(client, location_document)
         name = location_document["location_name"]
         logging.info(f"Added or updated location {name}.")
@@ -51,27 +57,27 @@ def create_or_update_branch_deployment(
 
 
 if __name__ == "__main__":
-    # # simple test entry point for add_or_update_code_location
-    # deployment_name, location_name, args = sys.argv[1:4]
-    # kwargs = dict(arg.split('=', 1) for arg in args.split(','))
-    # add_or_update_code_location(deployment_name, location_name, **kwargs)
+    # simple test entry point for add_or_update_code_location
+    deployment_name, location_name, args = sys.argv[1:4]
+    kwargs = dict(arg.split('=', 1) for arg in args.split(','))
+    add_or_update_code_location(deployment_name, location_name, **kwargs)
 
-    # simple test entry point for create_or_update_branch_deployment
-    from . import github_context
+    # # simple test entry point for create_or_update_branch_deployment
+    # from . import github_context
 
-    event = github_context.github_event()
-    print("Read github event:")
-    pprint.pprint(event.__dict__)
-    if not event.branch_name:
-        print("Not in a branch, not creating branch deployment")
-    else:
-        create_or_update_branch_deployment(
-            event.repo_name,
-            event.branch_name,
-            event.github_sha,
-            event.timestamp,
-            branch_url=event.branch_url,
-            pull_request_url=event.pull_request_url,
-            pull_request_status=event.pull_request_status,
-            pull_request_number=event.pull_request_id,
-        )
+    # event = github_context.github_event()
+    # print("Read github event:")
+    # pprint.pprint(event.__dict__)
+    # if not event.branch_name:
+    #     print("Not in a branch, not creating branch deployment")
+    # else:
+    #     create_or_update_branch_deployment(
+    #         event.repo_name,
+    #         event.branch_name,
+    #         event.github_sha,
+    #         event.timestamp,
+    #         branch_url=event.branch_url,
+    #         pull_request_url=event.pull_request_url,
+    #         pull_request_status=event.pull_request_status,
+    #         pull_request_number=event.pull_request_id,
+    #     )
