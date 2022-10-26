c4d06734fd3d0031e6fc557a4a18692cccfc96e8
prha <1040172+prha@users.noreply.github.com>
2022-10-26 08:52:39 -0700

    remove pip upgrade from template dockerfile (#49)
    * alter the dockerfile
    
    * bump version
    
    * remove pip upgrade
    
    * revert changes
    
    * Revert "revert changes"
    
    This reverts commit b9e60f0a32ee25247c0df0f905c6826acf996393.
    
    * upgrade pip before building action image

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,7 @@ class ExecContext:
         self.proc = None
 
     def tmp_file_path(self, filename) -> Path:
-        "Return a Path object pointing to named file in temp_dir."
+        "Return a Path object pointing to named file in tmp_dir."
         return Path(self.tmp_dir) / filename
 
     def tmp_file_content(self, filename):
@@ -53,7 +53,7 @@ class ExecContext:
             # write env vars
 
             for env_name, env_value in self.environ.items():
-                # map any tmp_dir paths to the target_temp_dir, useful for docker mounts
+                # map any tmp_dir paths to the target_tmp_dir, useful for docker mounts
                 tmp_dir_path = Path(self.tmp_dir)
                 if (
                     target_tmp_dir
