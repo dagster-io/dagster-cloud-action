80615b447cc9a57d20125c1d0f68f2407ff1083e
Shalabh Chaturvedi <shalabh@elementl.com>
2022-11-03 11:10:26 -0700

    Activate venv correctly

--- src/pex-builder/build-builder.sh
+++ src/pex-builder/build-builder.sh
@@ -15,22 +15,24 @@ BUILDER_PEX_PATH="${1:-build}/builder.pex"
 echo "Going to build $BUILDER_PEX_PATH"
 
 # create a venv to have a reproducible environment and not clobber the current python environment
-python3.8 -m venv build/venv
-source build/venv/activate
+VENV_ROOT=build/venv
+python3.8 -m venv $VENV_ROOT
+
+source $VENV_ROOT/bin/activate
 
 export PIPENV_IGNORE_VIRTUALENVS=1
-pip install pipenv pex
+$VENV_ROOT/bin/pip install pipenv pex
 
 # Build this python project. This will create the build/lib directory.
-python setup.py build
+$VENV_ROOT/bin/python3.8 setup.py build
 
 # Generate a requirements.txt from Pipfile.lock
 # Put it in build/lib so it also gets copied into the builder.pex for reference
-pipenv requirements --exclude-markers > build/lib/requirements.txt
+$VENV_ROOT/bin/pipenv requirements --exclude-markers > build/lib/requirements.txt
 
 # Generate a multi platform builder.pex (linux and macos)
 # Require running builder.pex under 3.8, in case multiple pythons are present on PATH
-pex -r build/lib/requirements.txt -D build/lib -o $BUILDER_PEX_PATH -v --include-tools \
+$VENV_ROOT/bin/pex -r build/lib/requirements.txt -D build/lib -o $BUILDER_PEX_PATH -v --include-tools \
     --python=python3.8 \
     --platform=manylinux2014_x86_64-cp-38-cp38 \
     --platform=macosx_12_0_x86_64-cp-38-cp38
