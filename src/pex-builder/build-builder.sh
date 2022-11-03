19700fa68d649deb932c2c9dba5696211bc3de77
Shalabh Chaturvedi <shalabh@elementl.com>
2022-11-03 13:04:03 -0700

    Add comment

--- src/pex-builder/build-builder.sh
+++ src/pex-builder/build-builder.sh
@@ -32,6 +32,7 @@ $VENV_ROOT/bin/pipenv requirements --exclude-markers > build/lib/requirements.tx
 
 # Generate a multi platform builder.pex (linux and macos)
 # Require running builder.pex under 3.8, in case multiple pythons are present on PATH
+# Use a PEX_ROOT to completely isolate this build from others that may be on this machine
 export PEX_ROOT=build/.pex
 $VENV_ROOT/bin/pex -r build/lib/requirements.txt -D build/lib -o $BUILDER_PEX_PATH -v --include-tools \
     --python=python3.8 \
