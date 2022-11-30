6986523cc8039105f51b8672e9a1fb95d8a24140
Shalabh Chaturvedi <shalabh@elementl.com>
2022-11-29 21:54:30 -0800

    Add --no-strip-pex-env flag

--- src/pex-builder/builder/github_context.py
+++ src/pex-builder/builder/github_context.py
@@ -107,7 +107,7 @@ def update_pr_comment(github_event: GithubEvent, action, deployment_name, locati
         return
 
     script_path = github_event.github_action_path.parent.parent / "src/create_or_update_comment.py"
-    if not script_path.exists:
+    if not script_path.exists():
         logging.warning("Did not find %r, not adding PR comment.", script_path)
 
     env = {name: value for name, value in os.environ.items() if not name.startswith("PEX_")}
