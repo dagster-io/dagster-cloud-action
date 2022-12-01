3fe180b53f9456ceeb92f84e690c73c7725af0b5
benpankow <ben@elementl.com>
2022-12-01 10:05:48 -0800

    add test

--- sample-repo/repo.py
+++ sample-repo/repo.py
@@ -1,7 +1,8 @@
 import csv
 import os
+
 import requests
-from dagster import asset, define_asset_job, job, op, repository
+from dagster import Definitions, asset, define_asset_job, job, op
 
 
 @asset
@@ -18,27 +19,26 @@ def nabisco_cereals(cereals):
     """Cereals manufactured by Nabisco"""
     return [row for row in cereals if row["mfr"] == "N"]
 
+
 @asset
 def prha_asset():
     return 1
 
+
 all_cereals_job = define_asset_job(name="all_cereals_job")
+
 
 @op
 def simple_op(context):
     value = os.getenv("FAKE_SECRET")
     context.log.info(f"GOT: {value}")
 
+
 @job
 def simple_job():
     simple_op()
 
-@repository
-def repo():
-    return [
-        cereals,
-        nabisco_cereals,
-        prha_asset,
-        all_cereals_job,
-        simple_job,
-    ]
+
+defs = Definitions(
+    assets=[cereals + nabisco_cereals + prha_asset], jobs=[all_cereals_job, simple_job]
+)
