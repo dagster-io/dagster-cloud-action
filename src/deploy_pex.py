497b0d14dfa517f5218ced9d20b8762ebea0d8bf
Shalabh Chaturvedi <shalabh@elementl.com>
2022-12-28 11:41:03 -0800

    debug

--- src/deploy_pex.py
+++ src/deploy_pex.py
@@ -149,6 +149,28 @@ def deploy_pex_from_docker(args):
 
 def fallback_to_docker_deploy():
     subprocess.run(["echo ENABLE_FAST_DEPLOYS=false >> $GITHUB_ENV"], shell=True)
+    import yaml, json
+    import os
+
+    workspace = os.environ['INPUT_DAGSTER_CLOUD_FILE']
+    secrets_set = bool(os.getenv('DAGSTER_CLOUD_API_TOKEN'))
+
+    with open(workspace) as f:
+        workspace_contents = f.read()
+    workspace_contents_yaml = yaml.safe_load(workspace_contents)
+
+    output_obj = [
+        {
+            'name': location['location_name'],
+            'directory': location.get('build', {'directory': '.'}).get('directory'),
+            'build_folder': location.get('build', {'directory': '.'}).get('directory'),
+            'registry': location.get('build', {'directory': '.'}).get('registry'),
+            'location_file': str(workspace),
+        }
+        for location in workspace_contents_yaml['locations']
+    ]
+    print(f'::set-output name=build_info::{json.dumps(output_obj)}')
+    print(f'::set-output name=secrets_set::{json.dumps(secrets_set)}')
 
 if __name__ == "__main__":
     fallback_to_docker_deploy()
