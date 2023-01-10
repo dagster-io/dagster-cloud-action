f9ca6d21591a3e2d262cb36bb4ec6d790536fbd4
Shalabh Chaturvedi <shalabh@elementl.com>
2023-01-09 20:06:07 -0500

    GitHub `ubuntu-22.04` support for pex files with sdists (#83)
    * Add new builder environment docker image and script to switch to using that on a ubuntu-22.04 github runner

--- src/pex-builder/setup.py
+++ src/pex-builder/setup.py
@@ -1,7 +1,22 @@
+import os
 from setuptools import setup
 
+
 if __name__ == "__main__":
+    PEX_BUILDER_SRC_DIR = os.getenv("PEX_BUILDER_SRC_DIR")
+    if PEX_BUILDER_SRC_DIR:
+        print(f"Using pex_builder from {PEX_BUILDER_SRC_DIR}")
+        files = os.listdir(PEX_BUILDER_SRC_DIR)
+        expected_files = {"deploy.py", "source.py", "deps.py"}
+        if {"deploy.py", "source.py", "deps.py"} & set(files) != expected_files:
+            raise SystemExit(
+                f"Did not find expected files {expected_files} in {PEX_BUILDER_SRC_DIR}"
+            )
+    else:
+        print(f"Using dagster-cloud-cli pex_builder from PyPI")
+        PEX_BUILDER_SRC_DIR = "./builder"
     setup(
         name="dagster_pex_builder",
         packages=["builder"],
+        package_dir={"builder": PEX_BUILDER_SRC_DIR},
     )
