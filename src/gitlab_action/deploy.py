0c485dcc1b276ebcc85af19e986a8108bbfab082
prha <1040172+prha@users.noreply.github.com>
2023-02-07 12:12:36 -0600

    Add hybrid/serverless template scripts for gitlab CI (#89)
    * Add remote workflow
    
    * Add commit-hash and git-url
    
    * Pass variables through yaml
    
    * grab branch name from correct env var for merge requests
    
    * update parse workspace script
    
    * add ci yml for serverless / hybrid
    
    * fix pex branch deployments
    
    * build serverless branch from image
    
    * recommit
    
    * fix url for pex branch deployments
    
    ---------
    
    Co-authored-by: Shalabh Chaturvedi <shalabh@elementl.com>
    Co-authored-by: Jordan Sanders <jordan@elementl.com>

---
+++ src/gitlab_action/deploy.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+import sys
+
+import parse_workspace
+
+
+def deploy(dagster_cloud_yaml_file, deployment=None):
+    url = os.environ['DAGSTER_CLOUD_URL']
+    if not os.getenv("SERVERLESS_BASE_IMAGE_PREFIX"):
+        base_image_prefix = "657821118200.dkr.ecr.us-west-2.amazonaws.com/dagster-cloud-serverless-base-"
+        if ".dogfood." in url:
+            base_image_prefix = "878483074102.dkr.ecr.us-west-2.amazonaws.com/dagster-cloud-serverless-base-"
+        os.environ["SERVERLESS_BASE_IMAGE_PREFIX"] = base_image_prefix
+
+    project = os.getenv("CI_PROJECT_NAME")
+    project_url = os.getenv("CI_PROJECT_URL")
+    commit = os.getenv("CI_COMMIT_SHORT_SHA")
+    branch = os.getenv("CI_COMMIT_BRANCH") or os.getenv("CI_COMMIT_REF_NAME")
+
+    deps_cache = project + "/" + branch
+    commit_url = project_url + "/commit/" + commit
+
+    locations = parse_workspace.get_locations(dagster_cloud_yaml_file)
+    assert not os.getenv("DISABLE_FAST_DEPLOYS")
+
+    for location in locations:
+        try:
+            print("Updating code location", location.name)
+            command_args = [
+                "dagster-cloud",
+                "serverless",
+                "deploy-python-executable",
+                f"--location-name={location.name}",
+                f"--location-file={dagster_cloud_yaml_file}",
+                f"--deps-cache-from={deps_cache}",
+                f"--deps-cache-to={deps_cache}",
+                f"--commit-hash={commit}",
+                f"--git-url={commit_url}",
+            ]
+            if deployment:
+                command_args.append(f"--url={url}/{deployment}")
+            subprocess.check_call(command_args, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as err:
+            print("Failed to update code location", location.name)
+            print(err.output)
+            sys.exit(1)
+
+
+if __name__ == "__main__":
+    dagster_cloud_yaml_file = sys.argv[1]
+    deployment = sys.argv[2] if len(sys.argv) > 2 else None
+    if os.path.exists(dagster_cloud_yaml_file):
+        deploy(dagster_cloud_yaml_file, deployment)
+    else:
+        print("Could not find dagster_cloud.yaml", dagster_cloud_yaml_file)
+        sys.exit(1)
---
+++ src/gitlab_action/parse_workspace.py
@@ -0,0 +1,55 @@
+import json
+import os
+import sys
+from dataclasses import asdict, dataclass
+from typing import List, Optional
+
+import yaml
+
+
+@dataclass
+class Location:
+    name: str
+    directory: str
+    build_folder: str
+    location_file: str
+    registry: Optional[str]
+
+
+def get_locations(dagster_cloud_yaml_file) -> List[Location]:
+    """Returns list of locations parsed from dagster_cloud.yaml."""
+    base_dir = os.path.abspath(os.path.dirname(dagster_cloud_yaml_file))
+
+    with open(dagster_cloud_yaml_file, encoding="utf-8") as yaml_file:
+        workspace_contents = yaml_file.read()
+        workspace_contents_yaml = yaml.safe_load(workspace_contents)
+
+        locations = []
+        for location in workspace_contents_yaml["locations"]:
+            build = location.get("build", {"directory": "."})
+            location_dir = os.path.join(base_dir, build.get("directory"))
+            locations.append(
+                Location(
+                    name=location["location_name"],
+                    directory=location_dir,
+                    build_folder=location_dir,
+                    location_file=os.path.abspath(dagster_cloud_yaml_file),
+                    registry= build.get("registry"),
+                )
+            )
+        return locations
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    locations = get_locations(filename)
+    location_names = [location.name for location in locations]
+    locations_by_name = { location.name: location for location in locations }
+    location_arg = sys.argv[2] if len(sys.argv) >= 3 else locations[0].name
+    location = locations_by_name.get(location_arg)
+    location_json = json.dumps(asdict(location))
+    print(f'DAGSTER_CLOUD_LOCATION={location_json}')
+    print(f'DAGSTER_CLOUD_LOCATION_NAME={location.name}')
+    print(f'DAGSTER_CLOUD_LOCATION_DIR={location.build_folder}')
+    if location.registry:
+        print(f'DAGSTER_CLOUD_LOCATION_REGISTRY={location.registry}')
