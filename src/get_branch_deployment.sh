d37142c2a0a49e3b21d076bddd1d9575a46504f6
prha <1040172+prha@users.noreply.github.com>
2023-04-05 14:57:30 -0700

    add action for getting the branch deployment name (#118)
    * add action for getting the branch deployment name
    
    * update references
    
    * make script executable
    
    * update to change add git permissions

---
+++ src/get_branch_deployment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -
+
+if [ -z $DAGSTER_CLOUD_URL ]; then
+    if [ -z $INPUT_DAGSTER_CLOUD_URL ]; then
+        export DAGSTER_CLOUD_URL="https://dagster.cloud/${INPUT_ORGANIZATION_ID}"
+    else
+        export DAGSTER_CLOUD_URL="${INPUT_DAGSTER_CLOUD_URL}"
+    fi
+fi
+
+if [ -z $INPUT_SOURCE_DIRECTORY ]; then
+    export INPUT_SOURCE_DIRECTORY=$(pwd)
+fi
+
+git config --global --add safe.directory /github/workspace
+BRANCH_DEPLOYMENT_NAME=$(dagster-cloud ci branch-deployment $INPUT_SOURCE_DIRECTORY)
+
+echo "deployment=${BRANCH_DEPLOYMENT_NAME}" >> $GITHUB_OUTPUT
