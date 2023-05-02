195f4fe64292757872e95281ead96b5837990725
Shalabh Chaturvedi <shalabh@elementl.com>
2023-05-01 19:55:12 -0700

    Rebuilt against master

--- src/pex-builder/build-dagster-cloud-pex.sh
+++ src/pex-builder/build-dagster-cloud-pex.sh
@@ -12,9 +12,17 @@ else
     export DAGSTER_CLOUD_CLI_SOURCE_PACKAGE="git+https://github.com/dagster-io/internal.git@${DAGSTER_CLOUD_INTERNAL_BRANCH}#egg=dagster-cloud-cli&subdirectory=dagster-cloud/python_modules/dagster-cloud-cli"
 fi
 
+if [ -z "$DAGSTER_BRANCH" ]
+then
+    export DAGSTER_SOURCE_PACKAGE="dagster"
+else
+    export DAGSTER_SOURCE_PACKAGE="git+https://github.com/dagster-io/dagster.git@${DAGSTER_BRANCH}#egg=dagster&subdirectory=python_modules/dagster"
+fi
+
 echo "Building dagster-cloud.pex from "
 echo " - $DAGSTER_CLOUD_SOURCE_PACKAGE"
 echo " - $DAGSTER_CLOUD_CLI_SOURCE_PACKAGE"
+echo " - $DAGSTER_SOURCE_PACKAGE"
 echo ""
 
 set -x
@@ -24,7 +32,7 @@ set -x
 # --pip-version and --resolver-version recommended by the pex team, uses the latest dependency
 #   resolution logic
 # --venv prepend unpacks the pex into a venv before execution, nice to have for debugging
-pex $DAGSTER_CLOUD_SOURCE_PACKAGE $DAGSTER_CLOUD_CLI_SOURCE_PACKAGE PyGithub \
+pex $DAGSTER_CLOUD_SOURCE_PACKAGE $DAGSTER_CLOUD_CLI_SOURCE_PACKAGE $DAGSTER_SOURCE_PACKAGE PyGithub \
     -o dagster-cloud.pex \
     --platform=manylinux2014_x86_64-cp-38-cp38 \
     --platform=macosx_12_0_x86_64-cp-38-cp38 \
