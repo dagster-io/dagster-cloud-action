fbc06f80df3fb32954c796f675062df333fc1cef
gibsondan <daniel@elementl.com>
2025-02-23 21:57:43 -0600

    fix get-branch-deployment action with a source_directory specified (#217)

--- src/get_branch_deployment.sh
+++ src/get_branch_deployment.sh
@@ -12,7 +12,8 @@ if [ -z $INPUT_SOURCE_DIRECTORY ]; then
     export INPUT_SOURCE_DIRECTORY=$(pwd)
 fi
 
-git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory $(realpath $INPUT_SOURCE_DIRECTORY)
+
 BRANCH_DEPLOYMENT_NAME=$(dagster-cloud ci branch-deployment $INPUT_SOURCE_DIRECTORY)
 
 echo "deployment=${BRANCH_DEPLOYMENT_NAME}" >> $GITHUB_OUTPUT
