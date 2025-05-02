21c35f46432f0bdb43b383b076a3832244154655
gibsondan <daniel@elementl.com>
2025-05-02 08:22:26 -0500

    Remove step from dagster-cloud-action release where you merge the release tag change back into master
    Summary:
    We haven't actually merged the release branch back into master in a while, and I can't think of a good reason to do so. This brings our release process in line with the OSS release process for other repos where there's a dev tag in master and we push from release branches.
    
    Test Plan:
    Run release script, verify resulting code changes

--- scripts/release.py
+++ scripts/release.py
@@ -22,6 +22,11 @@ DAGSTER_OSS_BRANCH_OPTION = typer.Option(
     envvar="DAGSTER_OSS_BRANCH",
     help="The OSS git branch that is used to build dagster and other packages.",
 )
+DAGSTER_OSS_VERSION_OPTION = typer.Option(
+    None,
+    envvar="DAGSTER_OSS_VERSION",
+    help="The version of the OSS dagster package to use.",
+)
 
 
 @contextmanager
@@ -83,6 +88,7 @@ def build_docker_action(version_tag: str, publish_docker_action: bool = True):
 @app.command(help="Build dagster-cloud.pex - invoked by the dagster-cloud-pex-builder image")
 def build_dagster_cloud_pex(
     dagster_oss_branch: Optional[str] = DAGSTER_OSS_BRANCH_OPTION,
+    dagster_oss_version: Optional[str] = DAGSTER_OSS_VERSION_OPTION,
 ):
     if dagster_oss_branch:
         info(f"Using dagster@{dagster_oss_branch} for dagster packages")
@@ -92,8 +98,9 @@ def build_dagster_cloud_pex(
         dagster_pipes_pkg = f"git+https://github.com/dagster-io/dagster.git@{dagster_oss_branch}#egg=dagster-pipes&subdirectory=python_modules/dagster-pipes"
         dagster_shared_pkg = f"git+https://github.com/dagster-io/dagster.git@{dagster_oss_branch}#egg=dagster-shared&subdirectory=python_modules/libraries/dagster-shared"
     else:
+        pin_str = f"=={dagster_oss_version}" if dagster_oss_version else ""
         info("Using PyPI for dagster package")
-        dagster_pkg = "dagster"
+        dagster_pkg = f"dagster{pin_str}"
         dagster_cloud_cli_pkg = "dagster-cloud-cli"
         dagster_dg_pkg = "dagster-dg"
         dagster_pipes_pkg = "dagster-pipes"
@@ -143,6 +150,7 @@ def build_dagster_cloud_pex(
 @app.command(help="Update dagster-cloud.pex")
 def update_dagster_cloud_pex(
     dagster_oss_branch: Optional[str] = DAGSTER_OSS_BRANCH_OPTION,
+    dagster_oss_version: Optional[str] = DAGSTER_OSS_VERSION_OPTION,
 ):
     # Map /generated on the docker image to our local generated folder
     map_folders = {"/generated": os.path.join(os.path.dirname(__file__), "..", "generated")}
@@ -150,6 +158,9 @@ def update_dagster_cloud_pex(
     env_args = []
     if dagster_oss_branch:
         env_args.extend(["-e", f"DAGSTER_OSS_BRANCH={dagster_oss_branch}"])
+
+    if dagster_oss_version:
+        env_args.extend(["-e", f"DAGSTER_OSS_VERSION={dagster_oss_version}"])
 
     mount_args = []
     for target_folder, source_folder in map_folders.items():
@@ -183,12 +194,11 @@ def update_dagster_cloud_pex(
 
 @app.command()
 def update_docker_action_references(
-    previous_version_tag,
     version_tag,
     glob_patterns: List[str] = ["**/*yaml", "**/*yml"],
 ):
     image_name = get_docker_action_image_name(version_tag)
-    previous_image_name = get_docker_action_image_name(previous_version_tag)
+    previous_image_name = get_docker_action_image_name("dev")
     info(f"Updating references from {previous_image_name} to {image_name}")
     with chdir("."):
         for pattern in glob_patterns:
@@ -204,26 +214,26 @@ def update_docker_action_references(
 @app.command()
 def create_rc(
     version_tag: str,
-    previous_version_tag: str,
     check_workdir: bool = True,
     execute_tests: bool = True,
     publish_docker_action: bool = True,
     dagster_oss_branch: Optional[str] = DAGSTER_OSS_BRANCH_OPTION,
+    dagster_oss_version: Optional[str] = DAGSTER_OSS_VERSION_OPTION,
 ):
     if check_workdir:
         ensure_clean_workdir()
     branch = ensure_in_branch()
     info(f"Preparing a new RC in {branch}")
 
-    if not re.match(r"^[0-9.]+$", version_tag):
+    if version_tag != "dev" and not re.match(r"^[0-9.]+$", version_tag):
         error(f"Invalid version tag {version_tag}")
         sys.exit(1)
 
-    update_dagster_cloud_pex(dagster_oss_branch)
+    update_dagster_cloud_pex(dagster_oss_branch, dagster_oss_version)
     if execute_tests:
         run_tests()
     build_docker_action(version_tag, publish_docker_action)
-    update_docker_action_references(previous_version_tag, version_tag)
+    update_docker_action_references(version_tag)
     info(f"Updated working directory for {version_tag}")
 
 
